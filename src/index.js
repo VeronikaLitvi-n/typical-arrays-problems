d6de75a89392393587dd1d386cdbed250c0cf73f
weronika-fed <nikafed3@gmail.com>
2021-08-23 22:41:33 +0300

    feat: implement agregated functions

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+exports.min = function min (array) {
+    if(array === undefined || array.length === 0){
+        return 0
+    }
+    let min = array[0];
+    for (i = 0; i< array.length; i++){
+        if (array[i] < min){
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+exports.max = function max (array) {
+    if(array === undefined || array.length === 0){
+        return 0
+    }
+    let max = array[0];
+    for (i = 0; i< array.length; i++){
+        if (array[i] > max){
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+exports.avg = function avg (array) {
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+    let sum = 0;
+    for (let i of array) sum += i;
+    
+    let avr = sum / array.length
+    return avr;
+}
+
+
